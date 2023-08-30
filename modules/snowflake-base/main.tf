504f085fdb22070374e02d390787b5fb3ac29366
gabriel <gabriel.candido@indicium.tech>
2023-08-30 14:46:06 -0300

    add provider in module

--- modules/snowflake-base/main.tf
+++ modules/snowflake-base/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source                = "Snowflake-Labs/snowflake"
+      version               = "0.59.0"
+      configuration_aliases = [snowflake.sys_admin, snowflake.security_admin, snowflake.account_admin]
+    }
+    snowsql = {
+      source                = "aidanmelen/snowsql"
+      version               = "1.3.3"
+      configuration_aliases = [snowsql.sys_admin, snowsql.security_admin, snowsql.account_admin]
+    }
+  }
+}
+
 resource "snowflake_warehouse" "warehouse" {
   provider                            = snowflake.account_admin
   name                                = var.warehouse_name
