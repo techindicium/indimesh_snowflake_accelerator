00f82ea7894aa3b73491a5c4c5e6685fc6fc25ec
gabriel <gabriel.candido@indicium.tech>
2023-08-30 10:35:38 -0300

    attach database and warehouse role to team role

--- modules/snowflake-base/roles.tf
+++ modules/snowflake-base/roles.tf
@@ -12,3 +12,39 @@ resource "snowflake_role" "team_role" {
   provider = snowflake.security_admin
   name     = var.team_role_name
 }
+
+resource "snowsql_exec" "grant_all_privileges_to_database" {
+  provider = snowsql.account_admin
+  create {
+    statements = <<-EOT
+    GRANT ALL PRIVILEGES ON DATABASE "${snowflake_database.database.name}" TO "${snowflake_role.database_role.name}";
+    EOT
+  }
+  delete {
+    statements = <<-EOT
+    REVOKE ALL PRIVILEGES ON DATABASE "${snowflake_database.database.name}" FROM "${snowflake_role.database_role.name}";
+    EOT
+  }
+}
+
+resource "snowsql_exec" "role_inheritance" {
+  provider = snowsql.account_admin
+  count    = var.create_optional_resource ? 1 : 0
+  create {
+    statements = <<-EOT
+    GRANT ROLE "${snowflake_role.database_role.name}" TO ROLE "${snowflake_role.team_role.name}";
+    GRANT ROLE "${snowflake_role.warehouse_role.name}" TO ROLE "${snowflake_role.team_role.name}";
+    EOT
+  }
+  delete {
+    statements = <<-EOT
+    REVOKE ROLE "${snowflake_role.database_role.name}" FROM ROLE "${snowflake_role.team_role.name}";
+    REVOKE ROLE "${snowflake_role.warehouse_role.name}" FROM ROLE "${snowflake_role.team_role.name}";
+    EOT
+  }
+  depends_on = [
+    snowflake_warehouse_grant.warehouse_usage,
+    snowflake_database_grant.database_usage,
+    snowsql_exec.grant_all_privileges_to_database
+  ]
+}
