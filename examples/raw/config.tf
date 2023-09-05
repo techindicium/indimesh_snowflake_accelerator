b7102912b882899dc07c68f69e4dc34e04d7d44d
gabriel <gabriel.candido@indicium.tech>
2023-09-05 15:06:29 -0300

    move data fo example folder

--- examples/raw/config.tf
+++ examples/raw/config.tf
@@ -1,4 +1,4 @@
 locals {
   wh_terraform = "TERRAFORM_WH"
-  raw_layer    = yamldecode(file("../../data/raw_layer.yaml"))
+  raw_layer    = yamldecode(file("../data//raw_layer.yaml"))
 }
--- examples/sandbox/config.tf
+++ examples/sandbox/config.tf
@@ -1,4 +1,4 @@
 locals {
   wh_terraform         = "TERRAFORM_WH"
-  config_sandbox_layer = yamldecode(file("../../data/sandbox_layer.yaml"))
+  config_sandbox_layer = yamldecode(file("../data//sandbox_layer.yaml"))
 }
