6ca19953290e9dd994f9464abd88da9dbcfeed68
gabriel <gabriel.candido@indicium.tech>
2023-09-14 18:26:37 -0300

    add and organize new test

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -8,6 +8,17 @@ install_terraform() {
     apt-get install terraform
 }
 
+install_tflint () {
+  curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
+}
+
+install_conftest () {
+  LATEST_VERSION=$(wget -O - "https://api.github.com/repos/open-policy-agent/conftest/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
+  wget "https://github.com/open-policy-agent/conftest/releases/download/v${LATEST_VERSION}/conftest_${LATEST_VERSION}_Linux_x86_64.tar.gz"
+  tar xzf conftest_${LATEST_VERSION}_Linux_x86_64.tar.gz
+  sudo mv conftest /usr/local/bin
+}
+
 run_terraform_init() {
     echo "${SNOWFLAKE_KEY_BASE64}" | base64 -d > "${BITBUCKET_CLONE_DIR}/snowflake_tf_snow_key.p8"
     chmod 600 snowflake_tf_snow_key.p8
@@ -15,15 +26,41 @@ run_terraform_init() {
     export SNOWFLAKE_PRIVATE_KEY_PATH="${BITBUCKET_CLONE_DIR}/snowflake_tf_snow_key.p8"
     cd "${BITBUCKET_CLONE_DIR}/tests"
     
-    terraform init -backend-config="key=${BACKEND_KEY}" -backend-config="bucket=${BACKEND_BUCKET}" -backend-config="region=${AWS_REGION}"
+    terraform init -backend-config="key=${BACKEND_KEY}" -backend-config="bucket=${BACKEND_BUCKET}" -backend-config="region=${AWS_REGION}" && terraform show -json ./tfplan > tfplan.json
+    cd -
 }
 
-run_go_test() {
+run_static_analysis_test() {
+  terraform validate
+  tflint --init 
+  tflint
+  conftest test ./tfplan.json -p policy/main.rego
+}
+
+run_integration_test() {
     go mod init snowflake-base
     go get github.com/gruntwork-io/terratest/modules/terraform
     go test snowflake_base_test.go
 }
 
-install_terraform
-run_terraform_init
-run_go_test
+STACKS=${1//,/ }
+
+for STACK in $STACKS; do
+  case "$STACK" in
+    "setup_enverioment")
+      install_terraform
+      install_tflint
+      install_conftest
+      ;;
+    "static_analysis_test")
+      run_static_analysis_test
+      ;;
+    "integration_test")
+      run_integration_test
+      ;;
+    *)
+      echo "Invalid stack: $STACK"
+      exit 1
+      ;;
+  esac
+done
