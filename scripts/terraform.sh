47cc04d2b68d159849e6e56e583bfeb2056d5014
gabriel <gabriel.candido@indicium.tech>
2023-09-14 18:43:50 -0300

    remove sudo from install_conftest function

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -16,7 +16,7 @@ install_conftest () {
   LATEST_VERSION=$(wget -O - "https://api.github.com/repos/open-policy-agent/conftest/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
   wget "https://github.com/open-policy-agent/conftest/releases/download/v${LATEST_VERSION}/conftest_${LATEST_VERSION}_Linux_x86_64.tar.gz"
   tar xzf conftest_${LATEST_VERSION}_Linux_x86_64.tar.gz
-  sudo mv conftest /usr/local/bin
+  mv conftest /usr/local/bin
 }
 
 run_terraform_init() {
