871bae673f68c26dee25dfb88974c0d75f49f816
gabriel <gabriel.candido@indicium.tech>
2023-09-15 15:20:31 -0300

    fix test path

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -3,23 +3,29 @@ run_terraform_init() {
     chmod 600 snowflake_tf_snow_key.p8
 
     export SNOWFLAKE_PRIVATE_KEY_PATH="${BITBUCKET_CLONE_DIR}/snowflake_tf_snow_key.p8"
-    cd "${BITBUCKET_CLONE_DIR}/tests"
-    
-    terraform init -backend-config="key=${BACKEND_KEY}" -backend-config="bucket=${BACKEND_BUCKET}" -backend-config="region=${AWS_REGION}"
+    terraform init \
+    -backend-config="key=${BACKEND_KEY}" \
+    -backend-config="bucket=${BACKEND_BUCKET}" \
+    -backend-config="region=${AWS_REGION}" \
+    -chdir="${BITBUCKET_CLONE_DIR}/tests"
 }
 
 run_static_analysis_test() {
+    cd "${BITBUCKET_CLONE_DIR}/tests"
     terraform validate
     tflint --init
     tflint
     terraform plan -out=tfplan && terraform show -json ./tfplan > tfplan.json
     conftest test ./tfplan.json -p policy/main.rego
+    cd -
 }
 
 run_integration_test() {
+    cd "${BITBUCKET_CLONE_DIR}/tests"
     go mod init snowflake-base
     go get github.com/gruntwork-io/terratest/modules/terraform
     go test snowflake_base_test.go
+    cd -
 }
 
 METHODS=${1//,/ }
