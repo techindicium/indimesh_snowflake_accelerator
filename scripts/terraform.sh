ef8a3ecdc44236ce6bf2002930d54e9b4b63d190
gabriel <gabriel.candido@indicium.tech>
2023-09-15 15:07:54 -0300

    fixing CI/CD creating a docker image

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -1,27 +1,3 @@
-install_terraform() {
-    apt-get update
-    apt-get install -y gnupg software-properties-common
-    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-    gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-    apt update
-    apt-get install terraform
-}
-
-install_tflint () {
-    wget https://github.com/terraform-linters/tflint/releases/download/v0.48.0/tflint_linux_amd64.zip \
-    && unzip tflint_linux_amd64.zip \
-    && mv tflint /usr/local/bin/ \
-    && rm tflint_linux_amd64.zip
-}
-
-install_conftest () {
-    LATEST_VERSION=$(wget -O - "https://api.github.com/repos/open-policy-agent/conftest/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
-    wget "https://github.com/open-policy-agent/conftest/releases/download/v${LATEST_VERSION}/conftest_${LATEST_VERSION}_Linux_x86_64.tar.gz"
-    tar xzf conftest_${LATEST_VERSION}_Linux_x86_64.tar.gz
-    mv conftest /usr/local/bin
-}
-
 run_terraform_init() {
     echo "${SNOWFLAKE_KEY_BASE64}" | base64 -d > "${BITBUCKET_CLONE_DIR}/snowflake_tf_snow_key.p8"
     chmod 600 snowflake_tf_snow_key.p8
@@ -30,25 +6,20 @@ run_terraform_init() {
     cd "${BITBUCKET_CLONE_DIR}/tests"
     
     terraform init -backend-config="key=${BACKEND_KEY}" -backend-config="bucket=${BACKEND_BUCKET}" -backend-config="region=${AWS_REGION}"
-    cd -
 }
 
 run_static_analysis_test() {
-    cd "${BITBUCKET_CLONE_DIR}/tests"
     terraform validate
     tflint --init
     tflint
     terraform plan -out=tfplan && terraform show -json ./tfplan > tfplan.json
     conftest test ./tfplan.json -p policy/main.rego
-    cd -
 }
 
 run_integration_test() {
-    cd "${BITBUCKET_CLONE_DIR}/tests"
     go mod init snowflake-base
     go get github.com/gruntwork-io/terratest/modules/terraform
     go test snowflake_base_test.go
-    cd -
 }
 
 METHODS=${1//,/ }
@@ -56,9 +27,6 @@ METHODS=${1//,/ }
 for TASK in $METHODS; do
     case "$TASK" in
         "setup_environment")
-          install_terraform
-          install_tflint
-          install_conftest
           run_terraform_init
           ;;
         "static_analysis_test")
