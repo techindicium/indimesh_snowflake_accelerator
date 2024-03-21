450ce46ca60303edbee90c6b023ecfe740234267
lucas zanotelli <lucas@zanotelli.dev>
2024-03-21 15:56:38 -0300

    run `terraform format`

--- examples/multiarea_setup/users.tf
+++ examples/multiarea_setup/users.tf
@@ -12,19 +12,19 @@ resource "snowflake_user" "user" {
   first_name   = each.value.first_name
   last_name    = each.value.last_anme
 
-  default_warehouse       = module.snowflake_warehouses[each.value.default_warehouse].warehouse_name
-  default_role            = module.custom_roles[each.value.default_role].custom_role_name
-  
+  default_warehouse = module.snowflake_warehouses[each.value.default_warehouse].warehouse_name
+  default_role      = module.custom_roles[each.value.default_role].custom_role_name
+
   # Iterate all roles created with custom_role module and return only those included in the
   # secondary_roles config
-  
+
   default_secondary_roles = [
     for index, custom_role in module.custom_roles : custom_role.custom_role_name
     if contains(each.value.secondary_roles, custom_role.custom_role_name)
   ]
 
   must_change_password = true
-  
+
   lifecycle {
     ignore_changes = [
       password,
@@ -39,13 +39,13 @@ resource "snowsql_exec" "grant_default_role" {
   create {
     statements = <<-EOT
     USE ROLE SECURITYADMIN;
-    GRANT ROLE "${each.value.default_role}" TO USER "${each.value.name}";
+    GRANT ROLE "${each.value.default_role}" TO USER ${each.value.name};
     EOT
   }
   delete {
     statements = <<-EOT
     USE ROLE SECURITYADMIN;
-    REVOKE ROLE "${each.value.default_role}" FROM USER "${each.value.name}";
+    REVOKE ROLE "${each.value.default_role}" FROM USER ${each.value.name};
     EOT
   }
 }
