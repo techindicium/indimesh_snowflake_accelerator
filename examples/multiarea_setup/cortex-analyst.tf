417955a6822ae40b953454c83628e8372f9367bb
Felipe Campos <felipe.campos@indicium.tech>
2025-05-13 09:41:38 -0300

    snowflake-base -> indimesh_snowflake_accelerator

--- examples/multiarea_setup/cortex-analyst.tf
+++ examples/multiarea_setup/cortex-analyst.tf
@@ -1,6 +1,6 @@
 module "cortex_analyst_access" {
   for_each = toset(local.config.cortex_analyst_roles)
-  source   = "../../snowflake-base/modules/cortex-analyst"
+  source   = "../../modules/cortex-analyst"
 
   providers = {
     snowflake.sys_admin      = snowflake.sys_admin
