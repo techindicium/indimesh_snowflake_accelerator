e9f40fb15b67124554933cc738d836dd10934bd3
LuisSaibro <luis.saibro@indicium.tech>
2025-09-11 15:11:35 -0300

    upgrading versions

--- modules/cortex-analyst/versions.tf
+++ modules/cortex-analyst/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source = "snowflakedb/snowflake"
-      version = "2.5.0" 
+      version = "2.6.0"
       configuration_aliases = [snowflake.sys_admin, snowflake.security_admin]
     }
   }
--- modules/storage-integration/versions.tf
+++ modules/storage-integration/versions.tf
@@ -9,7 +9,7 @@ terraform {
     }
     snowflake = {
       source = "snowflakedb/snowflake"
-      version = "2.5.0" 
+      version = "2.6.0"
       configuration_aliases = [snowflake.sys_admin]
     }
     aws = {
