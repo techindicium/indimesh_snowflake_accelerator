4e0587338d15ba80c263dac153d6313281a29edb
Thais Barros Alvim <thais.alvim@indicium.tech>
2025-12-19 08:25:45 -0300

    feat: add image repository module

---
+++ modules/spcs-image-repository/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source                = "snowflakedb/snowflake"
+      version               = "2.6.0"
+      configuration_aliases = [snowflake.sys_admin, snowflake.security_admin]
+    }
+  }
+}
